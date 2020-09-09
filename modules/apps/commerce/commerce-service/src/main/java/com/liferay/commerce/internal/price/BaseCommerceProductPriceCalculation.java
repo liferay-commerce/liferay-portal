f83d78f2a5ab7982388ca5b34f12937907795165
Igor Beslic <igor.beslic@liferay.com>
2020-09-09 11:55:00 +0200

    COMMERCE-4698 service - Reorganize - update method signature to take advantage of CommerceMoney

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/price/BaseCommerceProductPriceCalculation.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/price/BaseCommerceProductPriceCalculation.java
@@ -200,13 +200,27 @@ public abstract class BaseCommerceProductPriceCalculation
 	}
 
 	protected BigDecimal[] getUpdatedPrices(
-			BigDecimal unitPrice, BigDecimal promoPrice, BigDecimal finalPrice,
+			CommerceMoney unitPriceCommerceMoney,
+			CommerceMoney promoPriceCommerceMoney, BigDecimal finalPrice,
 			CommerceContext commerceContext,
 			List<CommerceOptionValue> commerceOptionValues)
 		throws PortalException {
 
 		if ((commerceOptionValues == null) || commerceOptionValues.isEmpty()) {
-			return new BigDecimal[] {unitPrice, promoPrice, finalPrice};
+			return _toPriceArray(
+				unitPriceCommerceMoney, promoPriceCommerceMoney, finalPrice);
+		}
+
+		BigDecimal promoPrice = BigDecimal.ZERO;
+
+		if (!promoPriceCommerceMoney.isEmpty()) {
+			promoPrice = promoPriceCommerceMoney.getPrice();
+		}
+
+		BigDecimal unitPrice = BigDecimal.ZERO;
+
+		if (!unitPriceCommerceMoney.isEmpty()) {
+			unitPrice = unitPriceCommerceMoney.getPrice();
 		}
 
 		for (CommerceOptionValue commerceOptionValue : commerceOptionValues) {
@@ -225,8 +239,7 @@ public abstract class BaseCommerceProductPriceCalculation
 
 					unitPrice = unitPrice.add(optionValuePrice);
 
-					if ((promoPrice != null) &&
-						CommerceBigDecimalUtil.gt(
+					if (CommerceBigDecimalUtil.gt(
 							promoPrice, BigDecimal.ZERO)) {
 
 						promoPrice = promoPrice.add(optionValuePrice);
@@ -267,7 +280,8 @@ public abstract class BaseCommerceProductPriceCalculation
 				else if (CommerceBigDecimalUtil.isZero(
 							optionValueUnitPromoPrice) &&
 						 CommerceBigDecimalUtil.gt(
-							 promoPrice, BigDecimal.ZERO)) {
+							 promoPriceCommerceMoney.getPrice(),
+							 BigDecimal.ZERO)) {
 
 					promoPrice = promoPrice.add(
 						optionValueUnitPrice.multiply(
@@ -290,7 +304,8 @@ public abstract class BaseCommerceProductPriceCalculation
 			}
 		}
 
-		return new BigDecimal[] {unitPrice, promoPrice, finalPrice};
+		return _toPriceArray(
+			unitPriceCommerceMoney, promoPriceCommerceMoney, finalPrice);
 	}
 
 	protected void setCommerceProductPriceWithTaxAmount(
@@ -565,6 +580,25 @@ public abstract class BaseCommerceProductPriceCalculation
 		}
 
 		return false;
+	}
+
+	private BigDecimal[] _toPriceArray(
+		CommerceMoney unitPriceCommerceMoney,
+		CommerceMoney promoPriceCommerceMoney, BigDecimal finalPrice) {
+
+		BigDecimal[] prices = new BigDecimal[3];
+
+		if (!unitPriceCommerceMoney.isEmpty()) {
+			prices[0] = unitPriceCommerceMoney.getPrice();
+		}
+
+		if (!promoPriceCommerceMoney.isEmpty()) {
+			prices[1] = promoPriceCommerceMoney.getPrice();
+		}
+
+		prices[2] = finalPrice;
+
+		return prices;
 	}
 
 	private void _validate(
