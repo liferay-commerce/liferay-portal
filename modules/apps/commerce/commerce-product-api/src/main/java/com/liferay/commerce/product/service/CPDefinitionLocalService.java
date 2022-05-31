0cf8605ea77948d86fb4ca8a408ff5a7371bd764
mahmoud.azzam <mahmoud.azzam@liferay.com>
2022-05-31 14:28:09 +0100

    COMMERCE-9047 - commerce-product-api - buildService

--- modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPDefinitionLocalService.java
+++ modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPDefinitionLocalService.java
@@ -210,7 +210,8 @@ public interface CPDefinitionLocalService
 		throws PortalException;
 
 	@Indexable(type = IndexableType.REINDEX)
-	public CPDefinition copyCPDefinition(long cpDefinitionId, long groupId)
+	public CPDefinition copyCPDefinition(
+			long userId, long cpDefinitionId, long groupId)
 		throws PortalException;
 
 	/**
