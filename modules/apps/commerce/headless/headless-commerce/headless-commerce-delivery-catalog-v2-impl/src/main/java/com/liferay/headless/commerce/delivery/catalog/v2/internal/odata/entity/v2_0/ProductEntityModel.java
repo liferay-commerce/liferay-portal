1064de27cfc62139703851e1db163d67ee82b2b1
Crescenzo Rega <crescenzo.rega@liferay.com>
2023-03-07 15:01:10 +0100

    COMMERCE-10726 - headless-commerce-delivery-catalog-v2-impl - Add exception mapper and entities class

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-v2-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/v2/internal/odata/entity/v2_0/ProductEntityModel.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.delivery.catalog.v2.internal.odata.entity.v2_0;
+
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.odata.entity.CollectionEntityField;
+import com.liferay.portal.odata.entity.DateTimeEntityField;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.odata.entity.IntegerEntityField;
+import com.liferay.portal.odata.entity.StringEntityField;
+
+import java.util.Map;
+
+/**
+ * @author Crescenzo Rega
+ */
+public class ProductEntityModel implements EntityModel {
+
+	public ProductEntityModel() {
+		_entityFieldsMap = EntityModel.toEntityFieldsMap(
+			new CollectionEntityField(
+				new StringEntityField(
+					"categoryIds", locale -> "assetCategoryIds")),
+			new CollectionEntityField(
+				new StringEntityField("tags", locale -> "assetTagNames")),
+			new IntegerEntityField("catalogId", locale -> "commerceCatalogId"),
+			new DateTimeEntityField(
+				"createDate",
+				locale -> Field.getSortableFieldName(Field.CREATE_DATE),
+				locale -> Field.CREATE_DATE),
+			new DateTimeEntityField(
+				"modifiedDate",
+				locale -> Field.getSortableFieldName(Field.MODIFIED_DATE),
+				locale -> Field.MODIFIED_DATE),
+			new StringEntityField(
+				"name", locale -> Field.getSortableFieldName("name")),
+			new StringEntityField("productType", locale -> "productTypeName"),
+			new IntegerEntityField("statusCode", locale -> Field.STATUS));
+	}
+
+	@Override
+	public Map<String, EntityField> getEntityFieldsMap() {
+		return _entityFieldsMap;
+	}
+
+	private final Map<String, EntityField> _entityFieldsMap;
+
+}
