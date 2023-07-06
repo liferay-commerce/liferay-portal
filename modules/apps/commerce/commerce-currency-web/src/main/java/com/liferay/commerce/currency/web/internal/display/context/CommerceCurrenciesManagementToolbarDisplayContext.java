5719019b969b65486a6b927fee8ca7585f20219f
mahmoud.azzam <mahmoud.azzam@liferay.com>
2023-07-06 13:08:04 +0100

    COMMERCE-10421 commerce-currency-web Overriding getClearResultsURL to fix the clear button

--- modules/apps/commerce/commerce-currency-web/src/main/java/com/liferay/commerce/currency/web/internal/display/context/CommerceCurrenciesManagementToolbarDisplayContext.java
+++ modules/apps/commerce/commerce-currency-web/src/main/java/com/liferay/commerce/currency/web/internal/display/context/CommerceCurrenciesManagementToolbarDisplayContext.java
@@ -21,6 +21,7 @@ import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItemListBuilder;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
@@ -79,6 +80,19 @@ public class CommerceCurrenciesManagementToolbarDisplayContext
 				dropdownItem.setQuickAction(true);
 			}
 		).build();
+	}
+
+	@Override
+	public String getClearResultsURL() {
+		return PortletURLBuilder.create(
+			getPortletURL()
+		).setKeywords(
+			StringPool.BLANK
+		).setParameter(
+			"orderByCol", getOrderByCol()
+		).setParameter(
+			"orderByType", getOrderByType()
+		).buildString();
 	}
 
 	@Override
