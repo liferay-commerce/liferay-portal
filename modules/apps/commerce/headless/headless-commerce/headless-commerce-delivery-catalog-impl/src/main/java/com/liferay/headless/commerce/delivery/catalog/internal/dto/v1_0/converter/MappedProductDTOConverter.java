afdbde5997bcbdb556715d7d5269d3cd62150db9
Alessio Rendina <asilcomando@gmail.com>
2023-07-14 15:47:01 +0200

    COMMERCE-9619 Refactor dto converters

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/dto/v1_0/converter/MappedProductDTOConverter.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/dto/v1_0/converter/MappedProductDTOConverter.java
@@ -354,8 +354,7 @@ public class MappedProductDTOConverter
 							_cpInstanceHelper.getCPDefinitionOptionValueRelsMap(
 								cpInstance.getCPDefinitionId(),
 								jsonArray.toString()),
-							_language.getLanguageId(
-								mappedProductDTOConverterContext.getLocale()));
+							_cpInstanceLocalService);
 					});
 				setThumbnail(
 					() -> {
