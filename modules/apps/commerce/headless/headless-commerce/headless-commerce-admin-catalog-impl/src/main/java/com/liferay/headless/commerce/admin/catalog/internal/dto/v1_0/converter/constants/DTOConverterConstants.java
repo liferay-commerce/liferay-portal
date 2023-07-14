26ed3f9c263a3ec2148d7b2c3a34c56187075373
Alessio Rendina <asilcomando@gmail.com>
2023-07-14 15:26:44 +0200

    COMMERCE-9619 Added new constants

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/dto/v1_0/converter/constants/DTOConverterConstants.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/dto/v1_0/converter/constants/DTOConverterConstants.java
@@ -16,6 +16,7 @@ package com.liferay.headless.commerce.admin.catalog.internal.dto.v1_0.converter.
 
 /**
  * @author Pedro Victor Silvestre
+ * @author Alessio Antonio Rendina
  */
 public class DTOConverterConstants {
 
@@ -26,6 +27,10 @@ public class DTOConverterConstants {
 	public static final String OPTION_CATEGORY_DTO_CONVERTER =
 		"(component.name=com.liferay.headless.commerce.admin.catalog." +
 			"internal.dto.v1_0.converter.OptionCategoryDTOConverter)";
+
+	public static final String PRODUCT_OPTION_VALUE_DTO_CONVERTER =
+		"(component.name=com.liferay.headless.commerce.admin.catalog." +
+			"internal.dto.v1_0.converter.ProductOptionValueDTOConverter)";
 
 	public static final String PRODUCT_SPECIFICATION_DTO_CONVERTER =
 		"(component.name=com.liferay.headless.commerce.admin.catalog." +
