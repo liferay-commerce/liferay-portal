40787e5ed9cd1fedc68c42ef8944d98163854448
Danny Situ <danny.situ@liferay.com>
2023-11-13 12:01:08 -0800

    COMMERCE-12658 buildRest

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-channel-impl/src/main/java/com/liferay/headless/commerce/admin/channel/internal/resource/v1_0/OpenAPIResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-channel-impl/src/main/java/com/liferay/headless/commerce/admin/channel/internal/resource/v1_0/OpenAPIResourceImpl.java
@@ -85,6 +85,8 @@ public class OpenAPIResourceImpl {
 
 	private final Set<Class<?>> _resourceClasses = new HashSet<Class<?>>() {
 		{
+			add(AccountAddressChannelResourceImpl.class);
+
 			add(ChannelResourceImpl.class);
 
 			add(OrderTypeResourceImpl.class);
