15862d892d171d26a100b061e45153e0823937b6
andrea.ale.sbarra <andrea.ale.sbarra@gmail.com>
2023-11-23 18:27:39 +0100

    COMMERCE-12776 new services for the new virtual order item file entry

---
+++ modules/apps/commerce/commerce-product-type-virtual-order-service/src/main/java/com/liferay/commerce/product/type/virtual/order/service/impl/CommerceVirtualOrderItemFileEntryServiceImpl.java
@@ -0,0 +1,106 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.product.type.virtual.order.service.impl;
+
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderItem;
+import com.liferay.commerce.product.type.virtual.order.model.CommerceVirtualOrderItem;
+import com.liferay.commerce.product.type.virtual.order.model.CommerceVirtualOrderItemFileEntry;
+import com.liferay.commerce.product.type.virtual.order.service.CommerceVirtualOrderItemLocalService;
+import com.liferay.commerce.product.type.virtual.order.service.base.CommerceVirtualOrderItemFileEntryServiceBaseImpl;
+import com.liferay.commerce.service.CommerceOrderItemLocalService;
+import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	property = {
+		"json.web.service.context.name=commerce",
+		"json.web.service.context.path=CommerceVirtualOrderItemFileEntry"
+	},
+	service = AopService.class
+)
+public class CommerceVirtualOrderItemFileEntryServiceImpl
+	extends CommerceVirtualOrderItemFileEntryServiceBaseImpl {
+
+	public CommerceVirtualOrderItemFileEntry
+			fetchCommerceVirtualOrderItemFileEntry(
+				long commerceVirtualOrderItemFileEntryId)
+		throws PortalException {
+
+		CommerceVirtualOrderItemFileEntry commerceVirtualOrderItemFileEntry =
+			commerceVirtualOrderItemFileEntryLocalService.
+				fetchCommerceVirtualOrderItemFileEntry(
+					commerceVirtualOrderItemFileEntryId);
+
+		if (commerceVirtualOrderItemFileEntry != null) {
+			CommerceVirtualOrderItem commerceVirtualOrderItem =
+				_commerceVirtualOrderItemLocalService.
+					getCommerceVirtualOrderItem(
+						commerceVirtualOrderItemFileEntry.
+							getCommerceVirtualOrderItemId());
+
+			CommerceOrderItem commerceOrderItem =
+				commerceVirtualOrderItem.getCommerceOrderItem();
+
+			_commerceOrderModelResourcePermission.check(
+				getPermissionChecker(), commerceOrderItem.getCommerceOrderId(),
+				ActionKeys.VIEW);
+		}
+
+		return commerceVirtualOrderItemFileEntry;
+	}
+
+	@Override
+	public CommerceVirtualOrderItemFileEntry
+			updateCommerceVirtualOrderItemFileEntry(
+				long commerceVirtualOrderItemFileEntryId, long fileEntryId,
+				String url, String version)
+		throws PortalException {
+
+		CommerceVirtualOrderItemFileEntry commerceVirtualOrderItemFileEntry =
+			commerceVirtualOrderItemFileEntryLocalService.
+				getCommerceVirtualOrderItemFileEntry(
+					commerceVirtualOrderItemFileEntryId);
+
+		CommerceVirtualOrderItem commerceVirtualOrderItem =
+			_commerceVirtualOrderItemLocalService.getCommerceVirtualOrderItem(
+				commerceVirtualOrderItemFileEntry.
+					getCommerceVirtualOrderItemId());
+
+		CommerceOrderItem commerceOrderItem =
+			commerceVirtualOrderItem.getCommerceOrderItem();
+
+		_commerceOrderModelResourcePermission.check(
+			getPermissionChecker(), commerceOrderItem.getCommerceOrderId(),
+			ActionKeys.UPDATE);
+
+		return commerceVirtualOrderItemFileEntryLocalService.
+			updateCommerceVirtualOrderItemFileEntry(
+				commerceVirtualOrderItemFileEntryId, fileEntryId, url, version);
+	}
+
+	@Reference
+	private CommerceOrderItemLocalService _commerceOrderItemLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.commerce.model.CommerceOrder)"
+	)
+	private ModelResourcePermission<CommerceOrder>
+		_commerceOrderModelResourcePermission;
+
+	@Reference
+	private CommerceVirtualOrderItemLocalService
+		_commerceVirtualOrderItemLocalService;
+
+}
