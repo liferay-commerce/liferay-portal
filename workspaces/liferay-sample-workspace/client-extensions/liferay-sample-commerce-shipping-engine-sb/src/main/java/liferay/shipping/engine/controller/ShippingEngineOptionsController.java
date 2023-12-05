a9f085b9232161dc485e7dcbfb473a56df27820c
Luca Pellizzon <luca.pellizzon@liferay.com>
2023-12-05 18:10:33 +0100

    COMMERCE-12880 shipping engin CX

---
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-commerce-shipping-engine-sb/src/main/java/liferay/shipping/engine/controller/ShippingEngineOptionsController.java
@@ -0,0 +1,88 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package liferay.shipping.engine.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import java.util.List;
+
+import liferay.shipping.engine.dto.ShippingEngineOption;
+import liferay.shipping.engine.util.ShippingEngineOptionsEnumSingleton;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Luca Pellizzon
+ */
+@RequestMapping("/shipping/engine/options")
+@RestController
+public class ShippingEngineOptionsController extends BaseRestController {
+
+	@PostMapping
+	public ResponseEntity<String> post(
+		@AuthenticationPrincipal Jwt jwt, @RequestBody String json) {
+
+		log(jwt, _log, json);
+
+		try {
+			JSONObject shippingOptionsJSONObject = new JSONObject();
+
+			JSONArray shippingOptionJSONArray = new JSONArray();
+
+			ShippingEngineOptionsEnumSingleton
+				shippingEngineOptionsEnumSingleton =
+					ShippingEngineOptionsEnumSingleton.INSTANCE.getInstance();
+
+			List<ShippingEngineOption> shippingEngineOptionList =
+				shippingEngineOptionsEnumSingleton.
+					getShippingEngineOptionList();
+
+			ObjectWriter objectWriter = new ObjectMapper(
+			).writer(
+			).withDefaultPrettyPrinter();
+
+			for (ShippingEngineOption shippingEngineOption :
+					shippingEngineOptionList) {
+
+				JSONObject shippingOptionJSONObject = new JSONObject(
+					objectWriter.writeValueAsString(shippingEngineOption));
+
+				shippingOptionJSONArray.put(shippingOptionJSONObject);
+			}
+
+			shippingOptionsJSONObject.put(
+				"shippingOptions", shippingOptionJSONArray);
+
+			log(jwt, _log, shippingOptionsJSONObject.toString());
+
+			return new ResponseEntity<>(
+				shippingOptionsJSONObject.toString(), HttpStatus.OK);
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+
+			return new ResponseEntity<>(json, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		ShippingEngineOptionsController.class);
+
+}
---
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-commerce-shipping-engine-sb/src/main/java/liferay/shipping/engine/dto/Account.java
@@ -0,0 +1,121 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package liferay.shipping.engine.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Luca Pellizzon
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Account {
+
+	@JsonProperty("emailAddress")
+	public String getEmailAddress() {
+		return _emailAddress;
+	}
+
+	@JsonProperty("externalReferenceCode")
+	public String getExternalReferenceCode() {
+		return _externalReferenceCode;
+	}
+
+	@JsonProperty("id")
+	public long getId() {
+		return _id;
+	}
+
+	@JsonProperty("logoId")
+	public long getLogoId() {
+		return _logoId;
+	}
+
+	@JsonProperty("name")
+	public String getName() {
+		return _name;
+	}
+
+	@JsonProperty("root")
+	public long getRoot() {
+		return _root;
+	}
+
+	@JsonProperty("taxId")
+	public long getTaxId() {
+		return _taxId;
+	}
+
+	@JsonProperty("type")
+	public int getType() {
+		return _type;
+	}
+
+	@JsonProperty("emailAddress")
+	public void setEmailAddress(String emailAddress) {
+		_emailAddress = emailAddress;
+	}
+
+	@JsonProperty("externalReferenceCode")
+	public void setExternalReferenceCode(String externalReferenceCode) {
+		_externalReferenceCode = externalReferenceCode;
+	}
+
+	@JsonProperty("id")
+	public void setId(long id) {
+		_id = id;
+	}
+
+	@JsonProperty("logoId")
+	public void setLogoId(long logoId) {
+		_logoId = logoId;
+	}
+
+	@JsonProperty("name")
+	public void setName(String name) {
+		_name = name;
+	}
+
+	@JsonProperty("root")
+	public void setRoot(long root) {
+		_root = root;
+	}
+
+	@JsonProperty("taxId")
+	public void setTaxId(long taxId) {
+		_taxId = taxId;
+	}
+
+	@JsonProperty("type")
+	public void setType(int type) {
+		_type = type;
+	}
+
+	@JsonProperty("emailAddress")
+	private String _emailAddress;
+
+	@JsonProperty("externalReferenceCode")
+	private String _externalReferenceCode;
+
+	@JsonProperty("id")
+	private long _id;
+
+	@JsonProperty("logoId")
+	private long _logoId;
+
+	@JsonProperty("name")
+	private String _name;
+
+	@JsonProperty("root")
+	private long _root;
+
+	@JsonProperty("taxId")
+	private long _taxId;
+
+	@JsonProperty("type")
+	private int _type;
+
+}
