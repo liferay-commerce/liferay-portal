b5b8946b640cc0a1f6bc714e1cfccfcc5aaaddf0
Joana Grego <joana.lima@liferay.com>
2024-03-20 17:31:51 -0300

    LPD-5877 Adjust post to methods with no content

--- modules/test/playwright/helpers/ApiHelpers.ts
+++ modules/test/playwright/helpers/ApiHelpers.ts
@@ -69,16 +69,21 @@ export class ApiHelpers {
 		this.dataEngine = new DataEngineApiHelper(this);
 		this.headlessAdminContent = new HeadlessAdminContentApiHelper(this);
 		this.headlessAdminUser = new HeadlessAdminUserApiHelper(this);
-		this.headlessCommerceAdminCatalog =
-			new HeadlessCommerceAdminCatalogApiHelper(this);
-		this.headlessCommerceAdminChannel =
-			new HeadlessCommerceAdminChannelApiHelper(this);
-		this.headlessCommerceAdminPaymentApiHelper =
-			new HeadlessCommerceAdminPaymentApiHelper(this);
-		this.headlessCommerceDeliveryCatalog =
-			new HeadlessCommerceDeliveryCatalogApiHelper(this);
-		this.headlessCommerceDeliveryCart =
-			new HeadlessCommerceDeliveryCartApiHelper(this);
+		this.headlessCommerceAdminCatalog = new HeadlessCommerceAdminCatalogApiHelper(
+			this
+		);
+		this.headlessCommerceAdminChannel = new HeadlessCommerceAdminChannelApiHelper(
+			this
+		);
+		this.headlessCommerceAdminPaymentApiHelper = new HeadlessCommerceAdminPaymentApiHelper(
+			this
+		);
+		this.headlessCommerceDeliveryCatalog = new HeadlessCommerceDeliveryCatalogApiHelper(
+			this
+		);
+		this.headlessCommerceDeliveryCart = new HeadlessCommerceDeliveryCartApiHelper(
+			this
+		);
 		this.headlessDelivery = new HeadlessDeliveryApiHelper(this);
 		this.headlessSite = new HeadlessSiteApiHelper(this);
 		this.jsonWebServicesClassName = new JSONWebServicesClassNameApiHelper(
@@ -119,6 +124,10 @@ export class ApiHelpers {
 			failOnStatusCode,
 			headers
 		);
+
+		if (response.status() === 204) {
+			return;
+		}
 
 		return response.json();
 	}
