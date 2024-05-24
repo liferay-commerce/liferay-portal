e8dc17c74b212dfa1890530a7110205aa7fedad3
Crescenzo Rega <crescenzo.rega@liferay.com>
2024-05-24 11:57:34 +0200

    LPD-20696 Add new mvc render command

---
+++ modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/action/EditCommerceReturnItemMVCRenderCommand.java
@@ -0,0 +1,106 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.order.web.internal.portlet.action;
+
+import com.liferay.account.service.AccountEntryLocalService;
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.currency.util.CommercePriceFormatter;
+import com.liferay.commerce.exception.NoSuchOrderException;
+import com.liferay.commerce.order.web.internal.display.context.CommerceReturnEditDisplayContext;
+import com.liferay.commerce.service.CommerceOrderItemLocalService;
+import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.list.type.service.ListTypeDefinitionService;
+import com.liferay.list.type.service.ListTypeEntryService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Crescenzo Rega
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + CommercePortletKeys.COMMERCE_RETURN,
+		"mvc.command.name=/commerce_return_item/edit_commerce_return_item"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditCommerceReturnItemMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			CommerceReturnEditDisplayContext commerceReturnEditDisplayContext =
+				new CommerceReturnEditDisplayContext(
+					_accountEntryLocalService, _commerceOrderLocalService,
+					_commerceOrderItemLocalService, _commercePriceFormatter,
+					_language, _listTypeDefinitionService,
+					_listTypeEntryService, _objectEntryLocalService,
+					_portal.getHttpServletRequest(renderRequest),
+					renderRequest);
+
+			renderRequest.setAttribute(
+				WebKeys.PORTLET_DISPLAY_CONTEXT,
+				commerceReturnEditDisplayContext);
+		}
+		catch (Exception exception) {
+			if (exception instanceof NoSuchOrderException ||
+				exception instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, exception.getClass());
+
+				return "/error.jsp";
+			}
+
+			throw new PortletException(exception);
+		}
+
+		return "/edit_commerce_return_item.jsp";
+	}
+
+	@Reference
+	private AccountEntryLocalService _accountEntryLocalService;
+
+	@Reference
+	private CommerceOrderItemLocalService _commerceOrderItemLocalService;
+
+	@Reference
+	private CommerceOrderLocalService _commerceOrderLocalService;
+
+	@Reference
+	private CommercePriceFormatter _commercePriceFormatter;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private ListTypeDefinitionService _listTypeDefinitionService;
+
+	@Reference
+	private ListTypeEntryService _listTypeEntryService;
+
+	@Reference
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
