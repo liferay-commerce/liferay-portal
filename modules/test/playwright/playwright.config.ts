0a6c554303f3846956d34c3336ccfff039e2ec93
Matyas Wollner <matyas.wollner@liferay.com>
2024-10-18 18:02:59 +0200

    LPD-39651 - Refactor LocalFile.CountriesManagement#CanActivateCountriesInBulk into Playwright

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -8,6 +8,7 @@ import {defineConfig, devices} from '@playwright/test';
 
 import {config as accessibilityMenuWeb} from './tests/accessibility-menu-web/config';
 import {config as accountAdminWebConfig} from './tests/account-admin-web/config';
+import {config as addressWebConfig} from './tests/address-web/config';
 import {config as analyticsReportsJsComponentsWeb} from './tests/analytics-reports-js-components-web/config';
 import {config as analyticsSettingsWebConfig} from './tests/analytics-settings-web/config';
 import {config as analyticsWebConfig} from './tests/analytics-web/config';
@@ -104,6 +105,7 @@ export default defineConfig({
 	projects: [
 		accessibilityMenuWeb,
 		accountAdminWebConfig,
+		addressWebConfig,
 		analyticsReportsJsComponentsWeb,
 		analyticsSettingsWebConfig,
 		analyticsWebConfig,
