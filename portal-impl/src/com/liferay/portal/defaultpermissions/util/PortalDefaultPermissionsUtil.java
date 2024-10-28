5fedbd179f821a1e0ab65aa0ebc179b3d2c4f769
Crescenzo Rega <crescenzo.rega@liferay.com>
2024-10-28 12:29:00 +0100

    LPD-35542 Change behavior to handle permission overwrites

--- portal-impl/src/com/liferay/portal/defaultpermissions/util/PortalDefaultPermissionsUtil.java
+++ portal-impl/src/com/liferay/portal/defaultpermissions/util/PortalDefaultPermissionsUtil.java
@@ -6,13 +6,24 @@
 package com.liferay.portal.defaultpermissions.util;
 
 import com.liferay.portal.defaultpermissions.kernel.configuration.manager.PortalDefaultPermissionsConfigurationManagerUtil;
+import com.liferay.portal.defaultpermissions.kernel.resource.PortalDefaultPermissionsModelResource;
+import com.liferay.portal.defaultpermissions.kernel.resource.PortalDefaultPermissionsModelResourceRegistry;
+import com.liferay.portal.defaultpermissions.kernel.resource.PortalDefaultPermissionsModelResourceRegistryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.AuditedModel;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.service.ResourceLocalServiceUtil;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.permission.ModelPermissions;
 import com.liferay.portal.kernel.service.permission.ModelPermissionsFactory;
+import com.liferay.portal.kernel.util.ListUtil;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -53,6 +64,60 @@ public class PortalDefaultPermissionsUtil {
 
 		ResourceLocalServiceUtil.updateModelResources(
 			auditedModel, serviceContext);
+
+		PortalDefaultPermissionsModelResourceRegistry
+			portalDefaultPermissionsModelResourceRegistry =
+				PortalDefaultPermissionsModelResourceRegistryUtil.
+					getPortalDefaultPermissionsModelResourceRegistry();
+
+		PortalDefaultPermissionsModelResource
+			portalDefaultPermissionsModelResource =
+				portalDefaultPermissionsModelResourceRegistry.
+					getPortalDefaultPermissionsModelResource(
+						auditedModel.getModelClassName());
+
+		if (portalDefaultPermissionsModelResource.
+				isAllowOverridePermissions()) {
+
+			_removeResourcePermission(
+				auditedModel, companyId,
+				ResourceActionsUtil.getModelResourceGuestDefaultActions(
+					auditedModel.getModelClassName()),
+				defaultPermissions, RoleConstants.GUEST);
+			_removeResourcePermission(
+				auditedModel, companyId,
+				ResourceActionsUtil.getModelResourceOwnerDefaultActions(
+					auditedModel.getModelClassName()),
+				defaultPermissions, RoleConstants.OWNER);
+			_removeResourcePermission(
+				auditedModel, companyId,
+				ResourceActionsUtil.getModelResourceGroupDefaultActions(
+					auditedModel.getModelClassName()),
+				defaultPermissions, RoleConstants.SITE_MEMBER);
+		}
+	}
+
+	private static void _removeResourcePermission(
+			AuditedModel auditedModel, long companyId,
+			List<String> defaultActions,
+			Map<String, String[]> defaultPermissions, String roleName)
+		throws PortalException {
+
+		List<String> actionIds = Arrays.asList(
+			defaultPermissions.getOrDefault(roleName, new String[0]));
+
+		Role role = RoleLocalServiceUtil.getRole(companyId, roleName);
+
+		for (String actionId : defaultActions) {
+			if (ListUtil.isEmpty(actionIds) || !actionIds.contains(actionId)) {
+				ResourceLocalServiceUtil.removeResourcePermission(
+					auditedModel.getCompanyId(),
+					auditedModel.getModelClassName(),
+					ResourceConstants.SCOPE_INDIVIDUAL,
+					String.valueOf(auditedModel.getPrimaryKeyObj()),
+					role.getRoleId(), actionId);
+			}
+		}
 	}
 
 }
