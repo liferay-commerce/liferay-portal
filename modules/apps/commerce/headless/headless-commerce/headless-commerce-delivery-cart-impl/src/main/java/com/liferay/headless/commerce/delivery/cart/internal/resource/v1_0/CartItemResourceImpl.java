224631a3977092e7301264e5371d1ab054b9fdae
Luca Pellizzon <luca.pellizzon@liferay.com>
2024-10-30 09:44:12 +0100

    LPD-35329 order item split when multishipping is enabled

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/resource/v1_0/CartItemResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/resource/v1_0/CartItemResourceImpl.java
@@ -434,7 +434,9 @@ public class CartItemResourceImpl extends BaseCartItemResourceImpl {
 
 		CommerceOrderItem commerceOrderItem;
 
-		if (commerceOrderCheckoutConfiguration.showSeparateOrderItems()) {
+		if (commerceOrderCheckoutConfiguration.showSeparateOrderItems() ||
+			commerceOrderCheckoutConfiguration.multishippingEnabled()) {
+
 			commerceOrderItem = _commerceOrderItemService.addCommerceOrderItem(
 				commerceOrder.getCommerceOrderId(), cartItem.getSkuId(),
 				cartItem.getOptions(),
