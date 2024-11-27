f15797d75e262a1c40fddce6a8121b698bf5224a
andrea.ale.sbarra <andrea.ale.sbarra@gmail.com>
2024-11-27 17:54:35 +0100

    LPD-42555 functional test

---
+++ modules/test/playwright/tests/commerce/commerce-product-definitions-web/commerceAdminProductConfigurations.spec.ts
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, mergeTests} from '@playwright/test';
+
+import {applicationsMenuPageTest} from '../../../fixtures/applicationsMenuPageTest';
+import {commercePagesTest} from '../../../fixtures/commercePagesTest';
+import {dataApiHelpersTest} from '../../../fixtures/dataApiHelpersTest';
+import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
+import {loginTest} from '../../../fixtures/loginTest';
+
+export const test = mergeTests(
+	applicationsMenuPageTest,
+	commercePagesTest,
+	dataApiHelpersTest,
+	featureFlagsTest({
+		'LPD-10889': true,
+	}),
+	loginTest()
+);
+
+test('LPD-42555 Verify configuration list table appears', async ({
+	applicationsMenuPage,
+	commerceAdminProductConfigurationListsPage,
+}) => {
+	await applicationsMenuPage.goToCommerceProductConfigurationLists(false);
+
+	await expect(
+		commerceAdminProductConfigurationListsPage.table
+	).toBeVisible();
+});
