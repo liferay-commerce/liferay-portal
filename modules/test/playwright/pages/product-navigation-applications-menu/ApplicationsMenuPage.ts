2f2813903bfec5035f7175480a31ede0a95b7d6b
Crescenzo Rega <crescenzo.rega@liferay.com>
2025-01-15 19:04:28 +0100

    LPD-45133 Fix functional tests

--- modules/test/playwright/pages/product-navigation-applications-menu/ApplicationsMenuPage.ts
+++ modules/test/playwright/pages/product-navigation-applications-menu/ApplicationsMenuPage.ts
@@ -17,6 +17,7 @@ export class ApplicationsMenuPage {
 	private readonly blueprintsItem: Locator;
 	private readonly clientExtensionsLink: Locator;
 	private readonly commerceChannelsMenuItem: Locator;
+	private readonly commerceCurrenciesMenuItem: Locator;
 	private readonly commerceDiscountsMenuItem: Locator;
 	private readonly commerceOrdersMenuItem: Locator;
 	private readonly commercePanelButton: Locator;
@@ -92,6 +93,10 @@ export class ApplicationsMenuPage {
 		this.commerceChannelsMenuItem = page.getByRole('menuitem', {
 			exact: true,
 			name: 'Channels',
+		});
+		this.commerceCurrenciesMenuItem = page.getByRole('menuitem', {
+			exact: true,
+			name: 'Currencies',
 		});
 		this.commerceDiscountsMenuItem = page.getByRole('menuitem', {
 			exact: true,
@@ -420,6 +425,11 @@ export class ApplicationsMenuPage {
 	async goToCommerceChannels(checkTabVisibility = true) {
 		await this.goToCommercePanel(checkTabVisibility);
 		await this.commerceChannelsMenuItem.click();
+	}
+
+	async goToCommerceCurrencies(checkTabVisibility = true) {
+		await this.goToCommercePanel(checkTabVisibility);
+		await this.commerceCurrenciesMenuItem.click();
 	}
 
 	async goToCommerceDiscounts() {
