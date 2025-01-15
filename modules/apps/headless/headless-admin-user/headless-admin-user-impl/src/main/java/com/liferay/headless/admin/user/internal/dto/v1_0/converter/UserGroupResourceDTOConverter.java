70a1ee4d752168f215fbef63d99ddfd45bcab580
brbalazs <balazs.breier@gmail.com>
2025-01-15 15:18:55 +0100

    LPD-44697 Added more info to getUserGroupsPage

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/UserGroupResourceDTOConverter.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/UserGroupResourceDTOConverter.java
@@ -5,15 +5,21 @@
 
 package com.liferay.headless.admin.user.internal.dto.v1_0.converter;
 
+import com.liferay.headless.admin.user.dto.v1_0.UserAccountBrief;
+import com.liferay.headless.admin.user.internal.dto.v1_0.util.CreatorUtil;
+import com.liferay.petra.function.transform.TransformUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.UserGroupService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
+import com.liferay.portal.vulcan.fields.NestedFieldsSupplier;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -61,10 +67,25 @@ public class UserGroupResourceDTOConverter
 		return new com.liferay.headless.admin.user.dto.v1_0.UserGroup() {
 			{
 				setActions(dtoConverterContext::getActions);
+				setCreator(
+					() -> NestedFieldsSupplier.supply(
+						"creator",
+						fieldName -> CreatorUtil.toCreator(
+							_portal,
+							_userLocalService.fetchUser(
+								userGroup.getUserId()))));
 				setDescription(userGroup::getDescription);
 				setExternalReferenceCode(userGroup::getExternalReferenceCode);
 				setId(userGroup::getUserGroupId);
 				setName(userGroup::getName);
+				setUserAccountBriefs(
+					() -> NestedFieldsSupplier.supply(
+						"userAccountBriefs",
+						fieldName -> TransformUtil.transformToArray(
+							_userLocalService.getUserGroupUsers(
+								userGroup.getUserGroupId()),
+							user -> _toUserAccountBrief(user),
+							UserAccountBrief.class)));
 				setUsersCount(
 					() -> _userLocalService.getUserGroupUsersCount(
 						userGroup.getUserGroupId(),
@@ -72,6 +93,21 @@ public class UserGroupResourceDTOConverter
 			}
 		};
 	}
+
+	private UserAccountBrief _toUserAccountBrief(User user) {
+		return new UserAccountBrief() {
+			{
+				setAlternateName(user::getScreenName);
+				setEmailAddress(user::getEmailAddress);
+				setExternalReferenceCode(user::getExternalReferenceCode);
+				setId(user::getUserId);
+				setName(user::getFullName);
+			}
+		};
+	}
+
+	@Reference
+	private Portal _portal;
 
 	@Reference
 	private UserGroupService _userGroupService;
