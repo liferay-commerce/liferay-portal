4d95ae40f870838f42e8c195bab69cffc4b6c998
Matyas Wollner <matyas.wollner@liferay.com>
2025-01-21 14:10:41 +0100

    LPD-44703 Fix previous test

--- modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/UserGroupResourceTest.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/UserGroupResourceTest.java
@@ -338,7 +338,7 @@ public class UserGroupResourceTest extends BaseUserGroupResourceTestCase {
 			null, "dateCreated ge " + dateFormat.format(date.getTime()),
 			Pagination.of(1, 2), null);
 
-		Assert.assertEquals(totalCount + 2, page.getTotalCount());
+		Assert.assertEquals(2, page.getTotalCount());
 
 		userGroup1.setDescription(
 			StringUtil.toLowerCase(RandomTestUtil.randomString()));
@@ -352,7 +352,7 @@ public class UserGroupResourceTest extends BaseUserGroupResourceTestCase {
 			null, "dateModified ge " + dateFormat.format(date.getTime()),
 			Pagination.of(1, 2), null);
 
-		Assert.assertEquals(totalCount + 1, page.getTotalCount());
+		Assert.assertEquals(1, page.getTotalCount());
 
 		assertContains(userGroup1, (List<UserGroup>)page.getItems());
 
