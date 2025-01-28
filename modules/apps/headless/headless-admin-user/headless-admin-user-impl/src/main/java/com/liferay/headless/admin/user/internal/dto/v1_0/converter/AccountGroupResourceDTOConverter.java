308decf3396f03089d00640866c424479a22b01b
Matyas Wollner <matyas.wollner@liferay.com>
2025-01-28 11:30:02 +0100

    LPD-44702 - Implement Export of Account Group Permissions

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/AccountGroupResourceDTOConverter.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/dto/v1_0/converter/AccountGroupResourceDTOConverter.java
@@ -15,13 +15,21 @@ import com.liferay.headless.admin.user.dto.v1_0.AccountGroup;
 import com.liferay.headless.admin.user.internal.dto.v1_0.util.CreatorUtil;
 import com.liferay.headless.admin.user.internal.dto.v1_0.util.CustomFieldsUtil;
 import com.liferay.petra.function.transform.TransformUtil;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.PermissionService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.vulcan.dto.converter.DTOConverter;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 import com.liferay.portal.vulcan.fields.NestedFieldsSupplier;
+import com.liferay.portal.vulcan.permission.Permission;
+import com.liferay.portal.vulcan.permission.PermissionUtil;
+
+import java.util.Collection;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -103,6 +111,33 @@ public class AccountGroupResourceDTOConverter
 					accountGroup::getExternalReferenceCode);
 				setId(accountGroup::getAccountGroupId);
 				setName(accountGroup::getName);
+				setPermissions(
+					() -> NestedFieldsSupplier.supply(
+						"permissions",
+						nestedFieldNames -> {
+							Company company = _companyLocalService.getCompany(
+								accountGroup.getCompanyId());
+
+							_permissionService.checkPermission(
+								company.getGroupId(),
+								com.liferay.account.model.AccountGroup.class.
+									getName(),
+								accountGroup.getAccountGroupId());
+
+							Collection<Permission> permissions =
+								PermissionUtil.getPermissions(
+									accountGroup.getCompanyId(),
+									_resourceActionLocalService.
+										getResourceActions(
+											com.liferay.account.model.
+												AccountGroup.class.getName()),
+									accountGroup.getAccountGroupId(),
+									com.liferay.account.model.AccountGroup.
+										class.getName(),
+									null);
+
+							return permissions.toArray(new Permission[0]);
+						}));
 			}
 		};
 	}
@@ -135,7 +170,16 @@ public class AccountGroupResourceDTOConverter
 	private AccountGroupRelLocalService _accountGroupRelLocalService;
 
 	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private PermissionService _permissionService;
+
+	@Reference
 	private Portal _portal;
+
+	@Reference
+	private ResourceActionLocalService _resourceActionLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
