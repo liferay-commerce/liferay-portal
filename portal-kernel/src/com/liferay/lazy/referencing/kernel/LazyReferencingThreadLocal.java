55f2df335d2b2dcd6ceac6311d341710c33f9297
Stefano Motta <117469730+smottal@users.noreply.github.com>
2025-02-26 10:11:28 +0100

    LPD-50006 New ThreadLocal

---
+++ portal-kernel/src/com/liferay/lazy/referencing/kernel/LazyReferencingThreadLocal.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.lazy.referencing.kernel;
+
+import com.liferay.petra.lang.CentralizedThreadLocal;
+
+/**
+ * @author Stefano Motta
+ */
+public class LazyReferencingThreadLocal {
+
+	public static boolean isIncompleteModel() {
+		return _incompleteModel.get();
+	}
+
+	public static boolean isLazyReferencingEnabled() {
+		return _lazyReferencingEnabled.get();
+	}
+
+	public static void setIncompleteModel(boolean incompleteModel) {
+		_incompleteModel.set(incompleteModel);
+	}
+
+	public static void setLazyReferencingEnabled(
+		boolean lazyReferencingEnabled) {
+
+		_lazyReferencingEnabled.set(lazyReferencingEnabled);
+	}
+
+	private static final ThreadLocal<Boolean> _incompleteModel =
+		new CentralizedThreadLocal<>(
+			LazyReferencingThreadLocal.class + "._incompleteModel",
+			() -> Boolean.FALSE);
+	private static final ThreadLocal<Boolean> _lazyReferencingEnabled =
+		new CentralizedThreadLocal<>(
+			LazyReferencingThreadLocal.class + "._lazyReferencingEnabled",
+			() -> Boolean.FALSE);
+
+}
