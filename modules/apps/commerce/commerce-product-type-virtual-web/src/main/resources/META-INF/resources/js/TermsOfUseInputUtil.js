21cf70773b7df237488cdc53812d8315ac6060d2
Christian Dorado <christian.dorado@liferay.com>
2025-07-08 14:56:49 -0700

    LPD-53601 Add conditions for feature flag for terms of use JavaScript

--- modules/apps/commerce/commerce-product-type-virtual-web/src/main/resources/META-INF/resources/js/TermsOfUseInputUtil.js
+++ modules/apps/commerce/commerce-product-type-virtual-web/src/main/resources/META-INF/resources/js/TermsOfUseInputUtil.js
@@ -4,150 +4,155 @@
  */
 
 export default function ({
-	isCKEditor5FeatureFlagEnabled,
-	termsOfUseJournalArticleBrowserURL,
-	useTermsOfUseJournal,
+    termsOfUseJournalArticleBrowserURL,
+    useTermsOfUseJournal,
 }) {
-	Liferay.Util.toggleBoxes(
-		`<portlet:namespace />termsOfUseRequired`,
-		`<portlet:namespace />termsOfUseSettings`
-	);
+    
+    Liferay.Util.toggleBoxes(
+        `<portlet:namespace />termsOfUseRequired`,
+        `<portlet:namespace />termsOfUseSettings`
+    );
 
-	const journalArticleNameInput = document.getElementById(
-		`<portlet:namespace />journalArticleNameInput`
-	);
+    const journalArticleNameInput = document.getElementById(
+        `<portlet:namespace />journalArticleNameInput`
+    );
 
-	const journalArticleRemove = document.getElementById(
-		`<portlet:namespace />journalArticleRemove`
-	);
+    const journalArticleRemove = document.getElementById(
+        `<portlet:namespace />journalArticleRemove`
+    );
 
-	const selectArticle = document.getElementById(
-		`<portlet:namespace />selectArticle`
-	);
+    const selectArticle = document.getElementById(
+        `<portlet:namespace />selectArticle`
+    );
 
-	if (journalArticleNameInput && journalArticleRemove && selectArticle) {
-		selectArticle.addEventListener('click', (event) => {
-			event.preventDefault();
+    if (journalArticleNameInput && journalArticleRemove && selectArticle) {
+        selectArticle.addEventListener('click', (event) => {
+            event.preventDefault();
 
-			Liferay.Util.openSelectionModal({
-				onSelect: (selectedItem) => {
-					const termsOfUseJournalArticleResourcePrimKey =
-						document.getElementById(
-							`<portlet:namespace />termsOfUseJournalArticleResourcePrimKey`
-						);
+            Liferay.Util.openSelectionModal({
+                onSelect: (selectedItem) => {
+                    const termsOfUseJournalArticleResourcePrimKey =
+                        document.getElementById(
+                            `<portlet:namespace />termsOfUseJournalArticleResourcePrimKey`
+                        );
 
-					const itemValue = JSON.parse(selectedItem.value);
+                    const itemValue = JSON.parse(selectedItem.value);
 
-					if (termsOfUseJournalArticleResourcePrimKey) {
-						termsOfUseJournalArticleResourcePrimKey.value =
-							itemValue.classPK;
-					}
+                    if (termsOfUseJournalArticleResourcePrimKey) {
+                        termsOfUseJournalArticleResourcePrimKey.value =
+                            itemValue.classPK;
+                    }
 
-					journalArticleRemove.classList.remove('hide');
+                    journalArticleRemove.classList.remove('hide');
 
-					journalArticleNameInput.innerText = itemValue.title;
-					if (isCKEditor5FeatureFlagEnabled) {
-						const ckEditorToolbar = document.querySelector(
-							'#termsOfUseContent .ck-editor__top'
-						);
-						ckEditorToolbar.style.display = 'none';
+                    journalArticleNameInput.innerText = itemValue.title;
+                    if (!Liferay.FeatureFlags['LPD-51378']) {
+                        const ckEditorToolbar = document.querySelector(
+                            '#termsOfUseContent .ck-editor__top'
+                        );
+                        ckEditorToolbar.style.display = 'none';
+    
+                        const ckEditorContent = document.querySelector(
+                            '#termsOfUseContent .ck-editor__main .ck-content'
+                        );
+                        ckEditorContent.classList.add('ck-read-only');
+                        ckEditorContent.setAttribute('contenteditable', false);
 
-						const ckEditorContent = document.querySelector(
-							'#termsOfUseContent .ck-editor__main .ck-content'
-						);
-						ckEditorContent.classList.add('ck-read-only');
-						ckEditorContent.setAttribute('contenteditable', false);
-					}
-				},
-				selectEventName: 'selectJournalArticle',
-				title: '<liferay-ui:message key="select-web-content" />',
-				url: termsOfUseJournalArticleBrowserURL,
-			});
-		});
+                    }
+                },
+                selectEventName: 'selectJournalArticle',
+                title: '<liferay-ui:message key="select-web-content" />',
+                url: termsOfUseJournalArticleBrowserURL,
+            });
+        });
 
-		journalArticleRemove.addEventListener('click', (event) => {
-			event.preventDefault();
+        journalArticleRemove.addEventListener('click', (event) => {
+            event.preventDefault();
 
-			const termsOfUseJournalArticleResourcePrimKey =
-				document.getElementById(
-					`<portlet:namespace />termsOfUseJournalArticleResourcePrimKey`
-				);
+            const termsOfUseJournalArticleResourcePrimKey =
+                document.getElementById(
+                    `<portlet:namespace />termsOfUseJournalArticleResourcePrimKey`
+                );
 
-			if (termsOfUseJournalArticleResourcePrimKey) {
-				termsOfUseJournalArticleResourcePrimKey.value = 0;
-			}
+            if (termsOfUseJournalArticleResourcePrimKey) {
+                termsOfUseJournalArticleResourcePrimKey.value = 0;
+            }
 
-			journalArticleNameInput.innerText =
-				'<liferay-ui:message key="none" />';
+            journalArticleNameInput.innerText =
+                '<liferay-ui:message key="none" />';
 
-			journalArticleRemove.classList.add('hide');
+            journalArticleRemove.classList.add('hide');
+            
+            if (!Liferay.FeatureFlags['LPD-51378']) {
+                const ckEditorToolbar = document.querySelector(
+                    '#termsOfUseContent .ck-editor__top'
+                );
+                ckEditorToolbar.style.display = 'block';
 
-			const ckEditorToolbar = document.querySelector(
-				'#termsOfUseContent .ck-editor__top'
-			);
-			ckEditorToolbar.style.display = 'block';
+                const ckEditorContent = document.querySelector(
+                    '#termsOfUseContent .ck-editor__main .ck-content'
+                );
+                ckEditorContent.classList.remove('ck-read-only');
+                ckEditorContent.setAttribute('contenteditable', true);
+            }
+        });
+    }
+    if (!Liferay.FeatureFlags['LPD-51378']) {
+        document.addEventListener('DOMContentLoaded', () => {
+            waitForElement('#termsOfUseContent .ck-editor__top').then(() => {
+                evaluateContentVisibility();
+            });
+        });
 
-			const ckEditorContent = document.querySelector(
-				'#termsOfUseContent .ck-editor__main .ck-content'
-			);
-			ckEditorContent.classList.remove('ck-read-only');
-			ckEditorContent.setAttribute('contenteditable', true);
-		});
-	}
+        Liferay.on('endNavigate', () => {
+            waitForElement('#termsOfUseContent .ck-editor__top').then(() => {
+                evaluateContentVisibility();
+            });
+        });
 
-	document.addEventListener('DOMContentLoaded', () => {
-		waitForElement('#termsOfUseContent .ck-editor__top').then(() => {
-			evaluateContentVisibility();
-		});
-	});
+        const evaluateContentVisibility = () => {
+            const ckEditorToolbar = document.querySelector(
+                '#termsOfUseContent .ck-editor__top'
+            );
 
-	Liferay.on('endNavigate', () => {
-		waitForElement('#termsOfUseContent .ck-editor__top').then(() => {
-			evaluateContentVisibility();
-		});
-	});
+            const hasTermsOfUseContent = useTermsOfUseJournal === 'true';
 
-	const evaluateContentVisibility = () => {
-		const ckEditorToolbar = document.querySelector(
-			'#termsOfUseContent .ck-editor__top'
-		);
+            if (hasTermsOfUseContent) {
+                ckEditorToolbar.style.display = 'none';
+                const ckEditorContent = document.querySelector(
+                    '#termsOfUseContent .ck-editor__main .ck-content'
+                );
+                ckEditorContent.classList.add('ck-read-only');
+                ckEditorContent.setAttribute('contenteditable', false);
+            }
+            else {
+                ckEditorToolbar.style.display = 'block';
+                const ckEditorContent = document.querySelector(
+                    '#termsOfUseContent .ck-editor__main .ck-content'
+                );
+                ckEditorContent.classList.remove('ck-read-only');
+                ckEditorContent.setAttribute('contenteditable', true);
+            }
+        };
 
-		const hasTermsOfUseContent = useTermsOfUseJournal === 'true';
+        const waitForElement = (selector) => {
+            return new Promise((resolve) => {
+                const selectedElement = document.querySelector(selector);
+                if (selectedElement) {
+                    return resolve(selectedElement);
+                }
 
-		if (hasTermsOfUseContent) {
-			ckEditorToolbar.style.display = 'none';
-			const ckEditorContent = document.querySelector(
-				'#termsOfUseContent .ck-editor__main .ck-content'
-			);
-			ckEditorContent.classList.add('ck-read-only');
-			ckEditorContent.setAttribute('contenteditable', false);
-		}
-		else {
-			ckEditorToolbar.style.display = 'block';
-			const ckEditorContent = document.querySelector(
-				'#termsOfUseContent .ck-editor__main .ck-content'
-			);
-			ckEditorContent.classList.remove('ck-read-only');
-			ckEditorContent.setAttribute('contenteditable', true);
-		}
-	};
+                const observer = new MutationObserver(() => {
+                    const pendingElement = document.querySelector(selector);
+                    if (pendingElement) {
+                        observer.disconnect();
+                        resolve(pendingElement);
+                    }
+                });
 
-	const waitForElement = (selector) => {
-		return new Promise((resolve) => {
-			const selectedElement = document.querySelector(selector);
-			if (selectedElement) {
-				return resolve(selectedElement);
-			}
-
-			const observer = new MutationObserver(() => {
-				const pendingElement = document.querySelector(selector);
-				if (pendingElement) {
-					observer.disconnect();
-					resolve(pendingElement);
-				}
-			});
-
-			observer.observe(document.body, {childList: true, subtree: true});
-		});
-	};
+                observer.observe(document.body, {childList: true, subtree: true});
+            });
+        };
+    }
 }
+
