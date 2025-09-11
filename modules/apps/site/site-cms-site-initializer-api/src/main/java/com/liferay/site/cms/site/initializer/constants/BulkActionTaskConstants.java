55026a128f80a904c101bf427b7ac908a9943cfb
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2025-09-11 12:44:41 +0200

    LPD-60079 Adds bulk action task constants

---
+++ modules/apps/site/site-cms-site-initializer-api/src/main/java/com/liferay/site/cms/site/initializer/constants/BulkActionTaskConstants.java
@@ -0,0 +1,37 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.cms.site.initializer.constants;
+
+/**
+ * @author Crescenzo Rega
+ */
+public class BulkActionTaskConstants {
+
+	public static final String BULK_ACTION_TASK_EXECUTION_STATUS_COMPLETED =
+		"completed";
+
+	public static final String BULK_ACTION_TASK_EXECUTION_STATUS_FAILED =
+		"failed";
+
+	public static final String BULK_ACTION_TASK_EXECUTION_STATUS_INITIAL =
+		"initial";
+
+	public static final String BULK_ACTION_TASK_EXECUTION_STATUS_STARTED =
+		"started";
+
+	public static final String
+		BULK_ACTION_TASK_ITEM_EXECUTION_STATUS_COMPLETED = "completed";
+
+	public static final String BULK_ACTION_TASK_ITEM_EXECUTION_STATUS_FAILED =
+		"failed";
+
+	public static final String BULK_ACTION_TASK_ITEM_EXECUTION_STATUS_INITIAL =
+		"initial";
+
+	public static final String BULK_ACTION_TASK_ITEM_EXECUTION_STATUS_STARTED =
+		"started";
+
+}
