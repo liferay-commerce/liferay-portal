8429c78dc49679e377f5eb8334b62e367a7d23dc
michelevigilante <michele.vigilante@liferay.com>
2025-11-05 15:03:38 +0100

    LPD-70010 Add ChannelNameExceptionMapper

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-channel-impl/src/main/java/com/liferay/headless/commerce/admin/channel/internal/jaxrs/exception/mapper/ChannelNameExceptionMapper.java
@@ -0,0 +1,42 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.commerce.admin.channel.internal.jaxrs.exception.mapper;
+
+import com.liferay.commerce.product.exception.CommerceChannelNameException;
+import com.liferay.headless.commerce.core.exception.mapper.BaseExceptionMapper;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Michele Vigilante
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Channel)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Channel.ChannelNameExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+@Provider
+public class ChannelNameExceptionMapper
+	extends BaseExceptionMapper<CommerceChannelNameException> {
+
+	@Override
+	public String getErrorDescription() {
+		return "Invalid channel name";
+	}
+
+	@Override
+	public Response.Status getStatus() {
+		return Response.Status.BAD_REQUEST;
+	}
+
+}
