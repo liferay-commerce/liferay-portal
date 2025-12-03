3d8882eacd061fc4e0337f252b9e177b5c293a51
smottal <117469730+smottal@users.noreply.github.com>
2025-12-03 18:17:40 +0100

    LPD-69509 DSR page

---
+++ modules/dxp/apps/digital-sales-room/digital-sales-room-web/src/main/resources/META-INF/resources/js/FDSPropsTransformer/DSRListFDSPropsTransformer.tsx
@@ -0,0 +1,50 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {openModal} from 'frontend-js-components-web';
+
+import {DSRInitializer} from '../index';
+
+export default function propsTransformer({
+	creationMenu,
+	...otherProps
+}: {
+	creationMenu: any;
+}) {
+	return {
+		...otherProps,
+		creationMenu: {
+			...creationMenu,
+			primaryItems: creationMenu.primaryItems.map(
+				(item: {data?: {action?: string}}) => {
+					return {
+						...item,
+						onClick() {
+							const action = item?.data?.action;
+
+							if (action && action === 'addDigitalSalesRoom') {
+								return openModal({
+									containerProps: {
+										className: '',
+									},
+									contentComponent: ({
+										closeModal,
+									}: {
+										closeModal: () => void;
+									}) =>
+										DSRInitializer({
+											closeModal,
+											numberOfSteps: 1,
+										}),
+									size: 'md',
+								});
+							}
+						},
+					};
+				}
+			),
+		},
+	};
+}
---
+++ modules/dxp/apps/digital-sales-room/digital-sales-room-web/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,24 @@
+<%--
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+ViewDigitalSalesRoomListDisplayContext viewDigitalSalesRoomListDisplayContext = (ViewDigitalSalesRoomListDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+%>
+
+<div class="portlet-digital-sales-room-management">
+	<frontend-data-set:headless-display
+		apiURL="<%= viewDigitalSalesRoomListDisplayContext.getAPIURL() %>"
+		creationMenu="<%= viewDigitalSalesRoomListDisplayContext.getCreationMenu() %>"
+		fdsActionDropdownItems="<%= viewDigitalSalesRoomListDisplayContext.getFDSActionDropdownItems() %>"
+		formName="fm"
+		id="<%= DigitalSalesRoomFDSNames.ROOMS %>"
+		propsTransformer="{DSRListFDSPropsTransformer} from digital-sales-room-web"
+		style="fluid"
+	/>
+</div>
