74941161a8e190c56a3d5cb79c0c694d61b53db9
smottal <117469730+smottal@users.noreply.github.com>
2025-12-16 09:40:38 +0100

    LPD-69528 Update usage

--- modules/dxp/apps/digital-sales-room/digital-sales-room-test-util/src/main/java/com/liferay/digital/sales/room/test/util/DigitalSalesRoomTestUtil.java
+++ modules/dxp/apps/digital-sales-room/digital-sales-room-test-util/src/main/java/com/liferay/digital/sales/room/test/util/DigitalSalesRoomTestUtil.java
@@ -5,21 +5,10 @@
 
 package com.liferay.digital.sales.room.test.util;
 
-import com.liferay.batch.engine.unit.BatchEngineUnitProcessor;
-import com.liferay.batch.engine.unit.BatchEngineUnitReader;
+import com.liferay.batch.engine.test.util.BatchEngineTestUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.service.ObjectDefinitionLocalServiceUtil;
-import com.liferay.portal.kernel.module.service.Snapshot;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-
-import java.io.File;
-
-import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
-
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
 
 /**
  * @author Stefano Motta
@@ -38,53 +27,21 @@ public class DigitalSalesRoomTestUtil {
 			return objectDefinition;
 		}
 
-		BatchEngineUnitProcessor batchEngineUnitProcessor =
-			_batchEngineUnitProcessorSnapshot.get();
-		BatchEngineUnitReader batchEngineUnitReader =
-			_batchEngineUnitReaderSnapshot.get();
-
-		Bundle testBundle = FrameworkUtil.getBundle(clazz);
-
-		BundleContext bundleContext = testBundle.getBundleContext();
-
-		for (Bundle bundle : bundleContext.getBundles()) {
-			if (!Objects.equals(
-					bundle.getSymbolicName(),
-					"com.liferay.digital.sales.room.impl")) {
-
-				continue;
-			}
-
-			_deleteFile(bundle, "01.object.folder");
-			_deleteFile(bundle, "02.object.definition");
-
-			CompletableFuture<Void> completableFuture =
-				batchEngineUnitProcessor.processBatchEngineUnits(
-					batchEngineUnitReader.getBatchEngineUnits(bundle));
-
-			completableFuture.join();
-		}
+		BatchEngineTestUtil.processBatchEngineUnits(
+			_BUNDLE_SYMBOLIC_NAME + ".impl", clazz,
+			new String[] {
+				"." + _BUNDLE_SYMBOLIC_NAME +
+					".internal.batch.01.object.folder",
+				"." + _BUNDLE_SYMBOLIC_NAME +
+					".internal.batch.02.object.definition"
+			});
 
 		return ObjectDefinitionLocalServiceUtil.
 			getObjectDefinitionByExternalReferenceCode(
 				"L_DSR_ROOM", TestPropsValues.getCompanyId());
 	}
 
-	private static void _deleteFile(Bundle bundle, String fileName) {
-		File file = bundle.getDataFile(
-			".com.liferay.digital.sales.room.internal.batch." + fileName +
-				".batch.engine.data.json.0.processed");
-
-		if ((file != null) && file.exists()) {
-			file.delete();
-		}
-	}
-
-	private static final Snapshot<BatchEngineUnitProcessor>
-		_batchEngineUnitProcessorSnapshot = new Snapshot<>(
-			DigitalSalesRoomTestUtil.class, BatchEngineUnitProcessor.class);
-	private static final Snapshot<BatchEngineUnitReader>
-		_batchEngineUnitReaderSnapshot = new Snapshot<>(
-			DigitalSalesRoomTestUtil.class, BatchEngineUnitReader.class);
+	private static final String _BUNDLE_SYMBOLIC_NAME =
+		"com.liferay.digital.sales.room";
 
 }
