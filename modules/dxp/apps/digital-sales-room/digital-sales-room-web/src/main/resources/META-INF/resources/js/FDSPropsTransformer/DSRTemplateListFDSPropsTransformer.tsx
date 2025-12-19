be10f7551d3c4f5adfd2d8ec876a3ec10874a1b2
smottal <117469730+smottal@users.noreply.github.com>
2025-12-19 11:50:51 +0100

    LPD-73189 DSR Create new template

---
+++ modules/dxp/apps/digital-sales-room/digital-sales-room-web/src/main/resources/META-INF/resources/js/FDSPropsTransformer/DSRTemplateListFDSPropsTransformer.tsx
@@ -0,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {IInternalRenderer} from '@liferay/frontend-data-set-web';
+import {openModal} from 'frontend-js-components-web';
+
+import DSRTemplateInitializer from '../components/DSRTemplateInitializer';
+import DSRTemplateNameRenderer from './cell_renderers/DSRTemplateNameRenderer';
+
+export default function propsTransformer({
+	creationMenu,
+	itemsActions = [],
+	...otherProps
+}: {
+	creationMenu: any;
+	itemsActions?: any[];
+}) {
+	return {
+		...otherProps,
+		creationMenu: {
+			...creationMenu,
+			primaryItems: creationMenu.primaryItems.map(
+				(item: {data?: {action?: string}}) => {
+					return {
+						...item,
+						onClick() {
+							const action = item?.data?.action;
+
+							if (
+								action &&
+								action === 'addDigitalSalesRoomTemplate'
+							) {
+								return openModal({
+									containerProps: {
+										className: '',
+									},
+									contentComponent: ({
+										closeModal,
+									}: {
+										closeModal: () => void;
+									}) =>
+										DSRTemplateInitializer({
+											closeModal,
+											numberOfSteps: 2,
+										}),
+									size: 'md',
+								});
+							}
+						},
+					};
+				}
+			),
+		},
+		customRenderers: {
+			tableCell: [
+				{
+					component: DSRTemplateNameRenderer,
+					name: 'name',
+					type: 'internal',
+				} as IInternalRenderer,
+			],
+		},
+		itemsActions,
+	};
+}
