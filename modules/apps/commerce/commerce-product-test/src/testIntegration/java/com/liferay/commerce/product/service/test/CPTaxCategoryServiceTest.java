364603ad0946ed59ee2174fd044d183fc450473d
Tancredi Covioli <tancredi.covioli@liferay.com>
2026-03-06 17:34:44 +0100

    LPD-78239 FIX remove lambdas in service test

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CPTaxCategoryServiceTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/service/test/CPTaxCategoryServiceTest.java
@@ -10,7 +10,6 @@ import com.liferay.commerce.product.constants.CPActionKeys;
 import com.liferay.commerce.product.model.CPTaxCategory;
 import com.liferay.commerce.product.service.CPTaxCategoryLocalService;
 import com.liferay.commerce.product.service.CPTaxCategoryService;
-import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.ResourceConstants;
@@ -63,92 +62,321 @@ public class CPTaxCategoryServiceTest {
 
 	@Test
 	public void testAddCPTaxCategory() throws Exception {
-		_runTest(
-			CPActionKeys.ADD_COMMERCE_PRODUCT_TAX_CATEGORY,
-			"com.liferay.commerce.tax", false,
-			() ->
-				_cpTaxCategory = _cpTaxCategoryService.addCPTaxCategory(
-					RandomTestUtil.randomString(),
-					RandomTestUtil.randomLocaleStringMap(),
-					RandomTestUtil.randomLocaleStringMap(),
-					ServiceContextTestUtil.getServiceContext()));
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategory = _cpTaxCategoryService.addCPTaxCategory(
+				RandomTestUtil.randomString(),
+				RandomTestUtil.randomLocaleStringMap(),
+				RandomTestUtil.randomLocaleStringMap(),
+				ServiceContextTestUtil.getServiceContext());
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				CPActionKeys.ADD_COMMERCE_PRODUCT_TAX_CATEGORY,
+				principalException.getMessage(), _user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), "com.liferay.commerce.tax",
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			CPActionKeys.ADD_COMMERCE_PRODUCT_TAX_CATEGORY);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategory = _cpTaxCategoryService.addCPTaxCategory(
+				RandomTestUtil.randomString(),
+				RandomTestUtil.randomLocaleStringMap(),
+				RandomTestUtil.randomLocaleStringMap(),
+				ServiceContextTestUtil.getServiceContext());
+		}
 	}
 
 	@Test
 	public void testCountCPTaxCategoriesByCompanyId() throws Exception {
-		_runTest(
-			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
-			"com.liferay.commerce.tax", true,
-			() -> _cpTaxCategoryService.countCPTaxCategoriesByCompanyId(
-				TestPropsValues.getCompanyId(), _cpTaxCategory.getName()));
+		_cpTaxCategory = _cpTaxCategoryLocalService.addCPTaxCategory(
+			RandomTestUtil.randomString(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			ServiceContextTestUtil.getServiceContext());
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.countCPTaxCategoriesByCompanyId(
+				TestPropsValues.getCompanyId(), _cpTaxCategory.getName());
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
+				principalException.getMessage(), _user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), "com.liferay.commerce.tax",
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.countCPTaxCategoriesByCompanyId(
+				TestPropsValues.getCompanyId(), _cpTaxCategory.getName());
+		}
 	}
 
 	@Test
 	public void testDeleteCPTaxCategory() throws Exception {
-		_runTest(
-			ActionKeys.DELETE, CPTaxCategory.class.getName(), true,
-			() -> _cpTaxCategoryService.deleteCPTaxCategory(
-				_cpTaxCategory.getCPTaxCategoryId()));
+		_cpTaxCategory = _cpTaxCategoryLocalService.addCPTaxCategory(
+			RandomTestUtil.randomString(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			ServiceContextTestUtil.getServiceContext());
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.deleteCPTaxCategory(
+				_cpTaxCategory.getCPTaxCategoryId());
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				ActionKeys.DELETE, principalException.getMessage(),
+				_user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), CPTaxCategory.class.getName(),
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			ActionKeys.DELETE);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.deleteCPTaxCategory(
+				_cpTaxCategory.getCPTaxCategoryId());
+		}
 	}
 
 	@Test
 	public void testFindCPTaxCategoriesByCompanyId() throws Exception {
-		_runTest(
-			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
-			"com.liferay.commerce.tax", true,
-			() -> _cpTaxCategoryService.findCPTaxCategoriesByCompanyId(
+		_cpTaxCategory = _cpTaxCategoryLocalService.addCPTaxCategory(
+			RandomTestUtil.randomString(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			ServiceContextTestUtil.getServiceContext());
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.findCPTaxCategoriesByCompanyId(
 				TestPropsValues.getCompanyId(), _cpTaxCategory.getName(),
-				QueryUtil.ALL_POS, QueryUtil.ALL_POS));
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
+				principalException.getMessage(), _user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), "com.liferay.commerce.tax",
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.findCPTaxCategoriesByCompanyId(
+				TestPropsValues.getCompanyId(), _cpTaxCategory.getName(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		}
 	}
 
 	@Test
 	public void testGetCPTaxCategories() throws Exception {
-		_runTest(
-			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
-			"com.liferay.commerce.tax", false,
-			() -> _cpTaxCategoryService.getCPTaxCategories(
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.getCPTaxCategories(
 				TestPropsValues.getCompanyId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS, null));
+				QueryUtil.ALL_POS, null);
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
+				principalException.getMessage(), _user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), "com.liferay.commerce.tax",
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.getCPTaxCategories(
+				TestPropsValues.getCompanyId(), QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS, null);
+		}
 	}
 
 	@Test
 	public void testGetCPTaxCategoriesCount() throws Exception {
-		_runTest(
-			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
-			"com.liferay.commerce.tax", false,
-			() -> _cpTaxCategoryService.getCPTaxCategoriesCount(
-				TestPropsValues.getCompanyId()));
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.getCPTaxCategoriesCount(
+				TestPropsValues.getCompanyId());
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
+				principalException.getMessage(), _user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), "com.liferay.commerce.tax",
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.getCPTaxCategoriesCount(
+				TestPropsValues.getCompanyId());
+		}
 	}
 
 	@Test
 	public void testGetCPTaxCategory() throws Exception {
-		_runTest(
-			ActionKeys.VIEW, CPTaxCategory.class.getName(), true,
-			() -> _cpTaxCategoryService.getCPTaxCategory(
-				_cpTaxCategory.getCPTaxCategoryId()));
+		_cpTaxCategory = _cpTaxCategoryLocalService.addCPTaxCategory(
+			RandomTestUtil.randomString(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			ServiceContextTestUtil.getServiceContext());
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.getCPTaxCategory(
+				_cpTaxCategory.getCPTaxCategoryId());
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				ActionKeys.VIEW, principalException.getMessage(),
+				_user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), CPTaxCategory.class.getName(),
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			ActionKeys.VIEW);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.getCPTaxCategory(
+				_cpTaxCategory.getCPTaxCategoryId());
+		}
 	}
 
 	@Test
 	public void testSearchCPTaxCategories() throws Exception {
-		_runTest(
-			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
-			"com.liferay.commerce.tax", true,
-			() -> _cpTaxCategoryService.searchCPTaxCategories(
+		_cpTaxCategory = _cpTaxCategoryLocalService.addCPTaxCategory(
+			RandomTestUtil.randomString(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			ServiceContextTestUtil.getServiceContext());
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.searchCPTaxCategories(
 				TestPropsValues.getCompanyId(), _cpTaxCategory.getName(),
 				QueryUtil.ALL_POS, QueryUtil.ALL_POS,
 				SortFactoryUtil.create(
-					Field.CREATE_DATE, Sort.STRING_TYPE, false)));
+					Field.CREATE_DATE, Sort.STRING_TYPE, false));
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES,
+				principalException.getMessage(), _user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), "com.liferay.commerce.tax",
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			CPActionKeys.VIEW_COMMERCE_PRODUCT_TAX_CATEGORIES);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.searchCPTaxCategories(
+				TestPropsValues.getCompanyId(), _cpTaxCategory.getName(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+				SortFactoryUtil.create(
+					Field.CREATE_DATE, Sort.STRING_TYPE, false));
+		}
 	}
 
 	@Test
 	public void testUpdateCPTaxCategory() throws Exception {
-		_runTest(
-			ActionKeys.UPDATE, CPTaxCategory.class.getName(), true,
-			() -> _cpTaxCategoryService.updateCPTaxCategory(
+		_cpTaxCategory = _cpTaxCategoryLocalService.addCPTaxCategory(
+			RandomTestUtil.randomString(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			ServiceContextTestUtil.getServiceContext());
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.updateCPTaxCategory(
 				_cpTaxCategory.getExternalReferenceCode(),
 				_cpTaxCategory.getCPTaxCategoryId(),
 				RandomTestUtil.randomLocaleStringMap(),
-				RandomTestUtil.randomLocaleStringMap()));
+				RandomTestUtil.randomLocaleStringMap());
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			_assertMessage(
+				ActionKeys.UPDATE, principalException.getMessage(),
+				_user.getUserId());
+		}
+
+		_resourcePermissionLocalService.addResourcePermission(
+			TestPropsValues.getCompanyId(), CPTaxCategory.class.getName(),
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
+			ActionKeys.UPDATE);
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				_user, PermissionCheckerFactoryUtil.create(_user))) {
+
+			_cpTaxCategoryService.updateCPTaxCategory(
+				_cpTaxCategory.getExternalReferenceCode(),
+				_cpTaxCategory.getCPTaxCategoryId(),
+				RandomTestUtil.randomLocaleStringMap(),
+				RandomTestUtil.randomLocaleStringMap());
+		}
 	}
 
 	private void _assertMessage(String actionKey, String message, long userId) {
@@ -157,43 +385,6 @@ public class CPTaxCategoryServiceTest {
 				StringBundler.concat(
 					"User ", userId, " must have ", actionKey,
 					" permission for")));
-	}
-
-	private void _runTest(
-			String actionId, String actionName, boolean createTaxCategory,
-			UnsafeRunnable<Exception> testRunnable)
-		throws Exception {
-
-		if (createTaxCategory) {
-			_cpTaxCategory = _cpTaxCategoryLocalService.addCPTaxCategory(
-				RandomTestUtil.randomString(),
-				RandomTestUtil.randomLocaleStringMap(),
-				RandomTestUtil.randomLocaleStringMap(),
-				ServiceContextTestUtil.getServiceContext());
-		}
-
-		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
-				_user, PermissionCheckerFactoryUtil.create(_user))) {
-
-			testRunnable.run();
-			Assert.fail();
-		}
-		catch (PrincipalException.MustHavePermission principalException) {
-			_assertMessage(
-				actionId, principalException.getMessage(), _user.getUserId());
-		}
-
-		_resourcePermissionLocalService.addResourcePermission(
-			TestPropsValues.getCompanyId(), actionName,
-			ResourceConstants.SCOPE_COMPANY,
-			String.valueOf(TestPropsValues.getCompanyId()), _role.getRoleId(),
-			actionId);
-
-		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
-				_user, PermissionCheckerFactoryUtil.create(_user))) {
-
-			testRunnable.run();
-		}
 	}
 
 	@DeleteAfterTestRun
