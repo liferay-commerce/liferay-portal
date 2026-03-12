c972e8e17a4fd365a310ae1bae47a8b870ea1244
smottal <117469730+smottal@users.noreply.github.com>
2026-03-12 15:34:25 +0100

    LPD-81993 Migrate tests

--- modules/test/playwright/tests/account-admin-web/main/accountEntriesManagementPortlet.spec.ts
+++ modules/test/playwright/tests/account-admin-web/main/accountEntriesManagementPortlet.spec.ts
@@ -10,6 +10,7 @@ import {applicationsMenuPageTest} from '../../../fixtures/applicationsMenuPageTe
 import {commercePagesTest} from '../../../fixtures/commercePagesTest';
 import {dataApiHelpersTest} from '../../../fixtures/dataApiHelpersTest';
 import {featureFlagsTest} from '../../../fixtures/featureFlagsTest';
+import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../fixtures/loginTest';
 import {usersAndOrganizationsPagesTest} from '../../../fixtures/usersAndOrganizationsPagesTest';
 import {getRandomInt} from '../../../utils/getRandomInt';
@@ -25,6 +26,7 @@ export const test = mergeTests(
 	featureFlagsTest({
 		'LPS-178052': {enabled: true},
 	}),
+	isolatedSiteTest,
 	loginTest(),
 	usersAndOrganizationsPagesTest
 );
@@ -37,14 +39,9 @@ test(
 		apiHelpers,
 		commerceAdminChannelsPage,
 		page,
+		site,
 	}) => {
 		test.setTimeout(120000);
-
-		const site = await apiHelpers.headlessSite.createSite({
-			name: getRandomString(),
-		});
-
-		apiHelpers.data.push({id: site.id, type: 'site'});
 
 		const layout = await apiHelpers.headlessDelivery.createSitePage({
 			pageDefinition: getPageDefinition([
@@ -116,6 +113,142 @@ test(
 		await expect(
 			await accountEntriesManagementPortletPage.accountEntriesTableRowSelectedCheck(
 				account1.name
+			)
+		).toBeVisible();
+	}
+);
+
+test(
+	'User can search for account',
+	{tag: ['@LPD-81993']},
+	async ({accountManagementWidgetPage, apiHelpers, page, site}) => {
+		const layout = await apiHelpers.headlessDelivery.createSitePage({
+			pageDefinition: getPageDefinition([
+				getWidgetDefinition({
+					id: getRandomString(),
+					widgetName:
+						'com_liferay_account_admin_web_internal_portlet_AccountEntriesManagementPortlet',
+				}),
+			]),
+			siteId: site.id,
+			title: getRandomString(),
+		});
+
+		const acmeIncName = `Acme Inc ${getRandomInt()}`;
+		const liferayName = `Liferay ${getRandomInt()}`;
+		const southBayName = `South Bay Auto Parts ${getRandomInt()}`;
+
+		for (const accountName of [acmeIncName, liferayName, southBayName]) {
+			await apiHelpers.headlessAdminUser.postAccount({
+				name: accountName,
+				type: 'business',
+			});
+		}
+
+		await page.goto(`/web/${site.name}/${layout.friendlyUrlPath}`);
+
+		await accountManagementWidgetPage.accountsTable.search(southBayName);
+
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(acmeIncName)
+		).not.toBeVisible();
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(liferayName)
+		).not.toBeVisible();
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(southBayName)
+		).toBeVisible();
+
+		await accountManagementWidgetPage.accountsTable.search(acmeIncName);
+
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(acmeIncName)
+		).toBeVisible();
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(liferayName)
+		).not.toBeVisible();
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(southBayName)
+		).not.toBeVisible();
+
+		await accountManagementWidgetPage.accountsTable.search('');
+
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(acmeIncName)
+		).toBeVisible();
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(liferayName)
+		).toBeVisible();
+		await expect(
+			accountManagementWidgetPage.accountsTable.cell(southBayName)
+		).toBeVisible();
+	}
+);
+
+test(
+	'User can set active account',
+	{tag: ['@LPD-81993']},
+	async ({accountEntriesManagementPortletPage, apiHelpers, page, site}) => {
+		const layout = await apiHelpers.headlessDelivery.createSitePage({
+			pageDefinition: getPageDefinition([
+				getWidgetDefinition({
+					id: getRandomString(),
+					widgetName:
+						'com_liferay_account_admin_web_internal_portlet_AccountEntriesManagementPortlet',
+				}),
+			]),
+			siteId: site.id,
+			title: getRandomString(),
+		});
+
+		const account1Name = `Account Name 1 ${getRandomInt()}`;
+		const account2Name = `Account Name 2 ${getRandomInt()}`;
+
+		const account1 = await apiHelpers.headlessAdminUser.postAccount({
+			name: account1Name,
+			type: 'business',
+		});
+
+		const account2 = await apiHelpers.headlessAdminUser.postAccount({
+			name: account2Name,
+			type: 'business',
+		});
+
+		const user = await apiHelpers.headlessAdminUser.postUserAccount();
+
+		await apiHelpers.headlessAdminUser.assignUserToAccountByEmailAddress(
+			account1.id,
+			[user.emailAddress]
+		);
+
+		await apiHelpers.headlessAdminUser.assignUserToAccountByEmailAddress(
+			account2.id,
+			[user.emailAddress]
+		);
+
+		await expect(async () => {
+			await page.goto(
+				`/web/${site.name}/${layout.friendlyUrlPath}?doAsUserId=${user.id}`
+			);
+
+			await expect(
+				accountEntriesManagementPortletPage.searchInput
+			).toBeEnabled({timeout: 2000});
+		}).toPass();
+
+		await expect(
+			await accountEntriesManagementPortletPage.accountEntriesTableRowSelectedCheck(
+				account1Name
+			)
+		).toBeVisible();
+
+		await accountEntriesManagementPortletPage.selectAccount(account2Name);
+
+		await page.reload();
+
+		await expect(
+			await accountEntriesManagementPortletPage.accountEntriesTableRowSelectedCheck(
+				account2Name
 			)
 		).toBeVisible();
 	}
