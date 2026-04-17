15aebf6dbc4dffe65f2a27addda19ec16720296a
Pedro Leite <pedro.leite@liferay.com>
2026-04-17 09:24:58 -0300

    LPD-86433 Add integration test

--- modules/apps/notification/notification-test/src/testIntegration/java/com/liferay/notification/internal/type/test/EmailNotificationTypeTest.java
+++ modules/apps/notification/notification-test/src/testIntegration/java/com/liferay/notification/internal/type/test/EmailNotificationTypeTest.java
@@ -14,6 +14,7 @@ import com.liferay.account.service.AccountEntryOrganizationRelLocalService;
 import com.liferay.account.service.AccountEntryUserRelLocalService;
 import com.liferay.account.service.AccountRoleLocalService;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.constants.CommerceOrderConstants;
 import com.liferay.commerce.constants.CommerceOrderPaymentConstants;
 import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
@@ -57,6 +58,7 @@ import com.liferay.object.constants.ObjectRelationshipConstants;
 import com.liferay.object.field.builder.AssigneeObjectFieldBuilder;
 import com.liferay.object.field.builder.PicklistObjectFieldBuilder;
 import com.liferay.object.field.builder.TextObjectFieldBuilder;
+import com.liferay.object.field.util.ObjectFieldUtil;
 import com.liferay.object.model.ObjectAction;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntryFolder;
@@ -413,16 +415,7 @@ public class EmailNotificationTypeTest extends BaseNotificationTypeTest {
 
 	@Test
 	public void testFreeMarkerNotificationWithCommerceOrder() throws Exception {
-		CommerceCurrency commerceCurrency =
-			CommerceCurrencyTestUtil.addCommerceCurrency(
-				TestPropsValues.getCompanyId());
-
-		CommerceChannel commerceChannel = CommerceTestUtil.addCommerceChannel(
-			TestPropsValues.getGroupId(), commerceCurrency.getCode());
-
-		CommerceOrder commerceOrder = CommerceTestUtil.addB2CCommerceOrder(
-			TestPropsValues.getUserId(), commerceChannel.getGroupId(),
-			commerceCurrency);
+		CommerceOrder commerceOrder = _addCommerceOrder();
 
 		commerceOrder = CommerceTestUtil.addCheckoutDetailsToCommerceOrder(
 			commerceOrder, TestPropsValues.getUserId(), true, true);
@@ -501,6 +494,58 @@ public class EmailNotificationTypeTest extends BaseNotificationTypeTest {
 			PrincipalThreadLocal.setName(originalName);
 
 			_objectActionLocalService.deleteObjectAction(objectAction);
+		}
+	}
+
+	@Test
+	public void testRichTextNotificationWithCommerceOrder() throws Exception {
+		ObjectDefinition commerceOrderObjectDefinition =
+			_objectDefinitionLocalService.fetchObjectDefinitionByClassName(
+				TestPropsValues.getCompanyId(), CommerceOrder.class.getName());
+
+		ObjectField objectField = ObjectFieldUtil.addCustomObjectField(
+			new TextObjectFieldBuilder(
+			).userId(
+				TestPropsValues.getUserId()
+			).objectDefinitionId(
+				commerceOrderObjectDefinition.getObjectDefinitionId()
+			).labelMap(
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString())
+			).name(
+				"a" + RandomTestUtil.randomString()
+			).build());
+
+		ObjectAction objectAction = _addNotificationTemplateObjectAction(
+			_getTermName(commerceOrderObjectDefinition, objectField.getName()),
+			NotificationTemplateConstants.EDITOR_TYPE_RICH_TEXT,
+			DestinationNames.COMMERCE_ORDER_STATUS,
+			commerceOrderObjectDefinition);
+
+		CommerceOrder commerceOrder = _addCommerceOrder();
+
+		try {
+			String objectFieldValue = RandomTestUtil.randomString();
+
+			_objectEntryLocalService.
+				addOrUpdateExtensionDynamicObjectDefinitionTableValues(
+					TestPropsValues.getUserId(), commerceOrderObjectDefinition,
+					commerceOrder.getCommerceOrderId(),
+					HashMapBuilder.<String, Serializable>put(
+						objectField.getName(), objectFieldValue
+					).build(),
+					ServiceContextTestUtil.getServiceContext());
+
+			commerceOrder = _commerceOrderEngine.transitionCommerceOrder(
+				commerceOrder, CommerceOrderConstants.ORDER_STATUS_PENDING,
+				TestPropsValues.getUserId(), true);
+
+			_assertNotificationQueueEntryBody(objectFieldValue);
+		}
+		finally {
+			_commerceOrderLocalService.deleteCommerceOrder(
+				commerceOrder.getCommerceOrderId());
+			objectActionLocalService.deleteObjectAction(objectAction);
+			objectFieldLocalService.deleteObjectField(objectField);
 		}
 	}
 
@@ -1714,6 +1759,19 @@ public class EmailNotificationTypeTest extends BaseNotificationTypeTest {
 			ObjectActionKeys.ADD_OBJECT_ENTRY);
 
 		return objectDefinition;
+	}
+
+	private CommerceOrder _addCommerceOrder() throws Exception {
+		CommerceCurrency commerceCurrency =
+			CommerceCurrencyTestUtil.addCommerceCurrency(
+				TestPropsValues.getCompanyId());
+
+		CommerceChannel commerceChannel = CommerceTestUtil.addCommerceChannel(
+			TestPropsValues.getGroupId(), commerceCurrency.getCode());
+
+		return CommerceTestUtil.addB2CCommerceOrder(
+			TestPropsValues.getUserId(), commerceChannel.getGroupId(),
+			commerceCurrency);
 	}
 
 	private NotificationTemplate _addNotificationTemplate(
