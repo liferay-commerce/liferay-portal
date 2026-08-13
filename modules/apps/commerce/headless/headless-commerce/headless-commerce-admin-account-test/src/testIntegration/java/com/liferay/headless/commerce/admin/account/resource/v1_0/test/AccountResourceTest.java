fe7892ce3df229a4eec47b10e126d1ca716d3d21
Tancredi Covioli <tancredi.covioli@liferay.com>
2026-08-13 18:24:09 +0200

    LPD-102137 Revert resource test

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-account-test/src/testIntegration/java/com/liferay/headless/commerce/admin/account/resource/v1_0/test/AccountResourceTest.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-account-test/src/testIntegration/java/com/liferay/headless/commerce/admin/account/resource/v1_0/test/AccountResourceTest.java
@@ -174,48 +174,6 @@ public class AccountResourceTest extends BaseAccountResourceTestCase {
 		Assert.assertTrue(false);
 	}
 
-	@Test
-	public void testPatchAccountWithInvalidName() throws Exception {
-		Account postAccount = randomAccount();
-
-		postAccount.setDefaultBillingAccountAddressId((Long)null);
-		postAccount.setDefaultShippingAccountAddressId((Long)null);
-
-		Account account = _postAccount(postAccount);
-
-		Account patchAccount = new Account() {
-			{
-				name = RandomTestUtil.randomString(251);
-			}
-		};
-
-		assertHttpResponseStatusCode(
-			400,
-			accountResource.patchAccountHttpResponse(
-				account.getId(), patchAccount));
-	}
-
-	@Test
-	public void testPatchAccountWithInvalidTaxIdNumber() throws Exception {
-		Account postAccount = randomAccount();
-
-		postAccount.setDefaultBillingAccountAddressId((Long)null);
-		postAccount.setDefaultShippingAccountAddressId((Long)null);
-
-		Account account = _postAccount(postAccount);
-
-		Account patchAccount = new Account() {
-			{
-				taxId = RandomTestUtil.randomString(76);
-			}
-		};
-
-		assertHttpResponseStatusCode(
-			400,
-			accountResource.patchAccountHttpResponse(
-				account.getId(), patchAccount));
-	}
-
 	@Ignore
 	@Override
 	@Test
@@ -237,38 +195,6 @@ public class AccountResourceTest extends BaseAccountResourceTestCase {
 	@Test
 	public void testPostAccountLogo() throws Exception {
 		Assert.assertTrue(false);
-	}
-
-	@Test
-	public void testPostAccountWithInvalidExternalReferenceCode()
-		throws Exception {
-
-		Account account = randomAccount();
-
-		account.setExternalReferenceCode(RandomTestUtil.randomString(76));
-
-		assertHttpResponseStatusCode(
-			400, accountResource.postAccountHttpResponse(account));
-	}
-
-	@Test
-	public void testPostAccountWithInvalidName() throws Exception {
-		Account account = randomAccount();
-
-		account.setName(RandomTestUtil.randomString(251));
-
-		assertHttpResponseStatusCode(
-			400, accountResource.postAccountHttpResponse(account));
-	}
-
-	@Test
-	public void testPostAccountWithInvalidTaxIdNumber() throws Exception {
-		Account account = randomAccount();
-
-		account.setTaxId(RandomTestUtil.randomString(76));
-
-		assertHttpResponseStatusCode(
-			400, accountResource.postAccountHttpResponse(account));
 	}
 
 	@Override
