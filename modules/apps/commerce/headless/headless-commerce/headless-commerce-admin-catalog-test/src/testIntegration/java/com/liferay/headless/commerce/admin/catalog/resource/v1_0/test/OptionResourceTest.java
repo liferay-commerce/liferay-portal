45a3e90bf6121f96ce6eba504d3c4fa462122638
michelevigilante <michele.vigilante@liferay.com>
2026-09-18 18:18:38 +0200

    LPD-93769 Test the product option endpoints

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/admin/catalog/resource/v1_0/test/OptionResourceTest.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/admin/catalog/resource/v1_0/test/OptionResourceTest.java
@@ -6,14 +6,224 @@
 package com.liferay.headless.commerce.admin.catalog.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.product.model.CPOption;
+import com.liferay.commerce.product.service.CPOptionLocalService;
+import com.liferay.headless.commerce.admin.catalog.client.dto.v1_0.Option;
+import com.liferay.headless.commerce.core.util.LanguageUtils;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Zoltán Takács
+ * @author Michele Vigilante
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class OptionResourceTest extends BaseOptionResourceTestCase {
+
+	@Ignore
+	@Override
+	@Test
+	public void testDeleteOption() throws Exception {
+		super.testDeleteOption();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testDeleteOptionByExternalReferenceCode() throws Exception {
+		super.testDeleteOptionByExternalReferenceCode();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLDeleteOption() throws Exception {
+		super.testGraphQLDeleteOption();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLDeleteOptionByExternalReferenceCode()
+		throws Exception {
+
+		super.testGraphQLDeleteOptionByExternalReferenceCode();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetOption() throws Exception {
+		super.testGraphQLGetOption();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetOptionByExternalReferenceCode() throws Exception {
+		super.testGraphQLGetOptionByExternalReferenceCode();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetOptionByExternalReferenceCodeNotFound()
+		throws Exception {
+
+		super.testGraphQLGetOptionByExternalReferenceCodeNotFound();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetOptionsPage() throws Exception {
+		super.testGraphQLGetOptionsPage();
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLPostOption() throws Exception {
+		super.testGraphQLPostOption();
+	}
+
+	@Override
+	@Test
+	public void testPatchOption() throws Exception {
+		Option randomOption = randomOption();
+
+		Option postOption = testPostOption_addOption(randomOption);
+
+		postOption.setKey(
+			StringUtil.toLowerCase(RandomTestUtil.randomString()));
+
+		optionResource.patchOption(postOption.getId(), postOption);
+
+		Option patchOption = optionResource.getOption(postOption.getId());
+
+		Assert.assertNotEquals(randomOption.getKey(), patchOption.getKey());
+
+		assertValid(postOption);
+	}
+
+	@Override
+	@Test
+	public void testPatchOptionByExternalReferenceCode() throws Exception {
+		Option randomOption = randomOption();
+
+		Option postOption = testPostOption_addOption(randomOption);
+
+		postOption.setKey(
+			StringUtil.toLowerCase(RandomTestUtil.randomString()));
+
+		optionResource.patchOptionByExternalReferenceCode(
+			postOption.getExternalReferenceCode(), postOption);
+
+		Option patchOption = optionResource.getOptionByExternalReferenceCode(
+			postOption.getExternalReferenceCode());
+
+		Assert.assertNotEquals(randomOption.getKey(), patchOption.getKey());
+
+		assertValid(postOption);
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {
+			"description", "facetable", "fieldType", "key", "name", "required",
+			"skuContributor"
+		};
+	}
+
+	@Override
+	protected String[] getIgnoredEntityFieldNames() {
+		return new String[] {"fieldType", "name"};
+	}
+
+	@Override
+	protected Option randomOption() throws Exception {
+		return new Option() {
+			{
+				description = LanguageUtils.getLanguageIdMap(
+					RandomTestUtil.randomLocaleStringMap());
+				externalReferenceCode = StringUtil.toLowerCase(
+					RandomTestUtil.randomString());
+				facetable = RandomTestUtil.randomBoolean();
+				fieldType = FieldType.SELECT;
+				key = StringUtil.toLowerCase(RandomTestUtil.randomString());
+				name = LanguageUtils.getLanguageIdMap(
+					RandomTestUtil.randomLocaleStringMap());
+				required = RandomTestUtil.randomBoolean();
+				skuContributor = RandomTestUtil.randomBoolean();
+			}
+		};
+	}
+
+	@Override
+	protected Option testDeleteOption_addOption() throws Exception {
+		return _addOption(randomOption());
+	}
+
+	@Override
+	protected Option testDeleteOptionByExternalReferenceCode_addOption()
+		throws Exception {
+
+		return _addOption(randomOption());
+	}
+
+	@Override
+	protected Option testGetOption_addOption() throws Exception {
+		return _addOption(randomOption());
+	}
+
+	@Override
+	protected Option testGetOptionByExternalReferenceCode_addOption()
+		throws Exception {
+
+		return _addOption(randomOption());
+	}
+
+	@Override
+	protected Option testGetOptionsPage_addOption(Option option)
+		throws Exception {
+
+		return _addOption(option);
+	}
+
+	@Override
+	protected Option testPostOption_addOption(Option option) throws Exception {
+		return _addOption(option);
+	}
+
+	@Override
+	protected Option testPutOptionByExternalReferenceCode_addOption()
+		throws Exception {
+
+		return _addOption(randomOption());
+	}
+
+	private Option _addOption(Option option) throws Exception {
+		Option postOption = optionResource.postOption(option);
+
+		_cpOptions.add(_cpOptionLocalService.getCPOption(postOption.getId()));
+
+		return postOption;
+	}
+
+	@Inject
+	private CPOptionLocalService _cpOptionLocalService;
+
+	@DeleteAfterTestRun
+	private final List<CPOption> _cpOptions = new ArrayList<>();
+
 }
